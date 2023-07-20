a78d323bfa7525615a044cf8f639c88bf997de52
Rafail Abdyushev <rafaelab@mail.ru>
2023-07-20 12:44:12 +0300

    class logger

--- MyLogger.py
+++ MyLogger.py
@@ -2,11 +2,11 @@ import logging
 
 class MyLogger:
 
-    FORMAT = '%(clientip)-15s %(message)s'
+    FORMAT = '%(clientip)-15s %(name)-4s %(message)s'
     # logging.basicConfig(format=FORMAT)
     handler = logging.FileHandler('app.log', 'w', 'utf-8')
     logger = logging.getLogger('taskStudent')
-    logger.setLevel('WARNING')
+    logger.setLevel(logging.DEBUG)
     formatter = logging.Formatter(FORMAT)
     handler.setFormatter(formatter)
     logger.addHandler(handler)
