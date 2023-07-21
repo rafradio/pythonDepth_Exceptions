3063b09c55b1bc5a77a1e8976b36d6d13186a048
Rafail Abdyushev <rafaelab@mail.ru>
2023-07-21 12:38:41 +0300

    class logger

--- MyLogger.py
+++ MyLogger.py
@@ -1,12 +1,18 @@
-import logging
+import logging, inspect
 
 class MyLogger:
-
-    FORMAT = '%(clientip)-15s %(name)-4s %(message)s'
-    # logging.basicConfig(format=FORMAT)
-    handler = logging.FileHandler('app.log', 'w', 'utf-8')
-    logger = logging.getLogger('taskStudent')
-    logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(FORMAT)
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    logger: logging.Logger = None
+    
+    @staticmethod
+    def configure():
+        FORMAT = '%(clientip)-15s %(name)-4s %(message)s'
+        # logging.basicConfig(format=FORMAT)
+        logger_name = inspect.stack()[1][3]
+        handler = logging.FileHandler('app.log', 'w', 'utf-8')
+        logger = logging.getLogger('taskStudent')
+        logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(FORMAT)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        MyLogger.logger = logger
+   
